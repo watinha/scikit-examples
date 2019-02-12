1907172c2c3980c28090401f5d65d401abc73867
Willian Massami Watanabe <watinha@gmail.com>
2019-02-12 15:26:26 -0200

    making things a little more organized

--- automatic_selection/xbis/parse_bib.py
+++ automatic_selection/xbis/parse_bib.py
@@ -4,12 +4,12 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 bib_filenames = [
-    'round1-google.bib',
-    'round1-ieee.bib',
-    'round1-outros.bib',
-    'round2-google.bib',
-    'round2-ieee.bib',
-    'round3-google.bib'
+    'bibs/round1-google.bib',
+    'bibs/round1-ieee.bib',
+    'bibs/round1-outros.bib',
+    'bibs/round2-google.bib',
+    'bibs/round2-ieee.bib',
+    'bibs/round3-google.bib'
 ]
 
 for file_index in range(6):
@@ -27,7 +27,7 @@ for file_index in range(6):
 
         for bib_index in range(len(titles)):
             insert = 'selecionado' if inserir[bib_index][1] == 'true' else 'removido'
-            newfile = codecs.open('%s/%s-%d.txt' % (folder, insert, (bib_index + (file_index * 1000))), 'w', encoding='utf-8')
+            newfile = codecs.open('corpus/%s/%s-%d.txt' % (folder, insert, (bib_index + (file_index * 1000))), 'w', encoding='utf-8')
             print('from %s writing file %s/%s-%d.txt' % (filename, folder, insert, (bib_index + (file_index * 1000))))
             abstract = re.sub('[\n\r]', ' ', abstracts[bib_index][1])
             content = u'%s\n%s' % (titles[bib_index][1], abstract)
