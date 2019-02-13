c87b1aafd6f4f803dce188285c8a79df6cd037fb
Willian Massami Watanabe <watinha@gmail.com>
2019-02-13 00:12:45 -0200

    refactoring: using class inheritance in classifiers

--- automatic_selection/xbis/pipeline/classifier.py
+++ automatic_selection/xbis/pipeline/classifier.py
@@ -3,53 +3,57 @@ import np, random
 from sklearn import tree, metrics, svm, naive_bayes
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 
-class DecisionTreeClassifier:
+class SimpleClassifier:
     def __init__ (self, seed):
         self._seed = seed
 
     def execute (self, dataset):
+        X = dataset['features']
+        y = dataset['categories']
+        random.seed(self._seed)
+        kfold = StratifiedKFold(n_splits=5, random_state=self._seed)
+        model = self.get_classifier()
+        scores = cross_val_score(model, X, y, cv=kfold, scoring='f1_macro')
+        print(scores)
+        print("OUR APPROACH F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
+        dataset['%s_scores' % self.classifier_name] = scores
+        return dataset
+
+class DecisionTreeClassifier (SimpleClassifier):
+    def __init__ (self, seed):
+        SimpleClassifier.__init__(self, seed)
+        self.classifier_name = 'decision_tree'
+
+    def get_classifier (self):
         print('===== Decision Tree Classifier =====')
-        X = dataset['features']
-        y = dataset['categories']
-        random.seed(self._seed)
-        model = tree.DecisionTreeClassifier(criterion='entropy', random_state=self._seed)
-        kfold = StratifiedKFold(n_splits=5, random_state=self._seed)
-        scores = cross_val_score(model, X, y, cv=kfold, scoring='f1_macro')
-        print(scores)
-        print("OUR APPROACH F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
-        dataset['decision_tree_scores'] = scores
-        return dataset
+        return tree.DecisionTreeClassifier(criterion='entropy', random_state=self._seed)
 
-class SVMClassifier:
+class SVMClassifier (SimpleClassifier):
     def __init__ (self, seed):
-        self._seed = seed
+        SimpleClassifier.__init__(self, seed)
+        self.classifier_name = 'svm'
 
-    def execute (self, dataset):
+    def get_classifier (self):
         print('===== SVM Classifier =====')
-        X = dataset['features']
-        y = dataset['categories']
-        random.seed(self._seed)
-        model = svm.SVC(gamma='scale')
-        kfold = StratifiedKFold(n_splits=5, random_state=self._seed)
-        scores = cross_val_score(model, X, y, cv=kfold, scoring='f1_macro')
-        print(scores)
-        print("OUR APPROACH F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
-        dataset['svm_scores'] = scores
-        return dataset
+        return svm.SVC(gamma='scale')
 
-class NaiveBayesClassifier:
+class NaiveBayesClassifier (SimpleClassifier):
     def __init__ (self, seed):
-        self._seed = seed
+        SimpleClassifier.__init__(self, seed)
+        self.classifier_name = 'naive_bayes'
+
+    def get_classifier (self):
+        print('===== NaiveBayes Classifier =====')
+        return naive_bayes.GaussianNB()
 
     def execute (self, dataset):
-        print('===== NaiveBayes Classifier =====')
         X = dataset['features']
         y = dataset['categories']
         random.seed(self._seed)
-        model = naive_bayes.GaussianNB()
         kfold = StratifiedKFold(n_splits=5, random_state=self._seed)
+        model = self.get_classifier()
         scores = cross_val_score(model, X.toarray(), y, cv=kfold, scoring='f1_macro')
         print(scores)
         print("OUR APPROACH F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
-        dataset['naive_bayes_scores'] = scores
+        dataset['%s_scores' % self.classifier_name] = scores
         return dataset
