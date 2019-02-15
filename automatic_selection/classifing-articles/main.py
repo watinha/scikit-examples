6963d24a1c994f9f07ca29f6566d853465793bbe
Willian Massami Watanabe <watinha@gmail.com>
2019-02-15 19:22:08 -0200

    now running the same pipeline in another dataset

--- automatic_selection/classifing-articles/main.py
+++ automatic_selection/classifing-articles/main.py
@@ -12,14 +12,24 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 argument = [
-    'bibs/ontologies/round1-google.bib',
-    'bibs/ontologies/round1-ieee.bib',
-    'bibs/ontologies/round1-outros.bib',
-    'bibs/ontologies/round2-google.bib',
-    'bibs/ontologies/round2-ieee.bib',
-    'bibs/ontologies/round3-google.bib'
+    'bibs/testing/round1-google.bib',
+    'bibs/testing/round1-ieee.bib',
+    'bibs/testing/round1-outros.bib',
+    'bibs/testing/round2-google.bib',
+    'bibs/testing/round2-ieee.bib',
+    'bibs/testing/round2-outros.bib',
+    'bibs/testing/round3-google.bib'
 ]
-project_folder = 'ontologies'
+project_folder = 'testing'
+#argument = [
+#    'bibs/ontologies/round1-google.bib',
+#    'bibs/ontologies/round1-ieee.bib',
+#    'bibs/ontologies/round1-outros.bib',
+#    'bibs/ontologies/round2-google.bib',
+#    'bibs/ontologies/round2-ieee.bib',
+#    'bibs/ontologies/round3-google.bib'
+#]
+#project_folder = 'ontologies'
 #argument = [
 #    'bibs/xbi/round1-google.bib',
 #    'bibs/xbi/round1-ieee.bib',
@@ -31,12 +41,12 @@ project_folder = 'ontologies'
 #project_folder = 'xbis'
 
 actions = [
-    BibParser(write_files=True, project_folder=project_folder),
+    BibParser(write_files=False, project_folder=project_folder),
     TextFilterComposite([ StopWordsFilter(), PorterStemmerFilter() ]),
     GenerateDataset(ngram_range=(1,3)),
-#    LSATransformation(n_components=100, random_state=42),
-    VarianceThresholdFeatureSelection(threshold=0.0001),
-    RFECVFeatureSelection(LinearSVC()),
+    LSATransformation(n_components=100, random_state=42),
+#    VarianceThresholdFeatureSelection(threshold=0.0001),
+#    RFECVFeatureSelection(LinearSVC()),
     DecisionTreeClassifier(42),
     SVMClassifier(42),
     LinearSVMClassifier(42),
