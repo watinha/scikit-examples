363a334e51ddfd5e95d61883b0dc06c630c30406
Willian Massami Watanabe <watinha@gmail.com>
2019-02-15 17:31:43 -0200

    trying feature selection to xbis problem

--- xbis/experiment.py
+++ xbis/experiment.py
@@ -1,18 +1,33 @@
 import arff, np, random
-from sklearn import tree, metrics
+from sklearn import tree, metrics, svm
 from sklearn.model_selection import cross_val_score, GroupKFold
+from sklearn.feature_selection import RFECV
+from sklearn.model_selection import StratifiedKFold
 
 features = [
-    'size',
-    'diff.height.height',
-    'diff.width.viewport',
-    'diff.left.viewport',
-    'diff.right.viewport',
-    'diff.parent.y',
-    'imageDiff.size',
-    'chiSquared.size',
-    'phash',
-    'childsNumber', 'textLength'
+'childsNumber', 'textLength', # all features
+'baseDPI', 'targetDPI', 'baseX',
+'targetX', 'baseY', 'targetY', 'baseHeight', 'targetHeight', 'baseWidth',
+'targetWidth', 'baseParentX', 'targetParentX', 'baseParentY', 'targetParentY', 'imageDiff',
+'chiSquared', 'baseDeviceWidth', 'targetDeviceWidth', 'baseViewportWidth', 'targetViewportWidth',
+'phash', 'crosscheck.base.a', 'crosscheck.target.a', 'crosscheck.SDR',
+'crosscheck.disp', 'crosscheck.area', 'crosscheck.LDTD', 'baseLeft', 'targetLeft', 'baseRight',
+'targetRight', 'baseParentLeft', 'targetParentLeft', 'baseParentRight', 'targetParentRight',
+'diff.viewport', 'size', 'diff.left.relation', 'diff.right.relation', 'diff.parent.left.relation',
+'diff.parent.right.relation', 'diff.left.viewport', 'diff.right.viewport', 'alignment',
+'diff.parent.left.viewport', 'diff.parent.right.viewport', 'parent.alignment', 'diff.parent.y',
+'diff.parent.y.height', 'diff.height.height', 'diff.width.viewport', 'imageDiff.size',
+'chiSquared.size', 'imageDiff.size.viewport', 'chiSquared.size.viewport', 'pHashDistance.viewport',
+    #'size',  // features used in the article
+    #'diff.height.height',
+    #'diff.width.viewport',
+    #'diff.left.viewport',
+    #'diff.right.viewport',
+    #'diff.parent.y',
+    #'imageDiff.size',
+    #'chiSquared.size',
+    #'phash',
+    #'childsNumber', 'textLength'
 ]
 
 crosscheck_features = [
@@ -58,3 +73,24 @@ folds = GroupKFold(n_splits=10)
 scores = cross_val_score(model, X_cross, y, cv=folds, groups=urls, scoring='f1_macro')
 print(scores)
 print("CROSSCHECK   F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
+print('===== Feature selection =====')
+#model = tree.DecisionTreeClassifier(criterion='entropy')
+model = svm.LinearSVC()
+rfecv = RFECV(model, cv=StratifiedKFold(5), scoring='f1_macro')
+X_new = rfecv.fit_transform(X, y, groups=urls)
+print (X.shape)
+print (X_new.shape)
+print (features_index)
+print (rfecv.ranking_)
+random.seed(42)
+#model = tree.DecisionTreeClassifier(criterion='entropy', random_state=42)
+model = svm.SVC(gamma='scale')
+model = svm.LinearSVC()
+folds = GroupKFold(n_splits=10)
+scores = cross_val_score(model, X_new, y, cv=folds, groups=urls, scoring='f1_macro')
+print(scores)
+print("Our approach Feature Selection SVM F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
+features.append('diff_out_viewport_left')
+features.append('diff_out_viewport_right')
+selected_index = [ features[i] for i in range(len(features)) if rfecv.ranking_[i] == 1]
+print(selected_index)
