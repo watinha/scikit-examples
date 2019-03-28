31c52db3371631c525d95f53a71af97f373655d8
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2019-03-28 20:50:26 -0300

    adding hyperparameter tunning to scripts

--- xbis/experiment.py
+++ xbis/experiment.py
@@ -6,19 +6,18 @@ from sklearn.model_selection import StratifiedKFold
 
 
 features = [
-'childsNumber', 'textLength', # all features
-'baseDPI', 'targetDPI', 'baseX',
+'baseDPI', 'targetDPI', 'baseX', # all features
 'targetX', 'baseY', 'targetY', 'baseHeight', 'targetHeight', 'baseWidth',
 'targetWidth', 'baseParentX', 'targetParentX', 'baseParentY', 'targetParentY', 'imageDiff',
 'chiSquared', 'baseDeviceWidth', 'targetDeviceWidth', 'baseViewportWidth', 'targetViewportWidth',
-'phash', 'crosscheck.base.a', 'crosscheck.target.a', 'crosscheck.SDR',
+'crosscheck.base.a', 'crosscheck.target.a', 'crosscheck.SDR',
 'crosscheck.disp', 'crosscheck.area', 'crosscheck.LDTD', 'baseLeft', 'targetLeft', 'baseRight',
 'targetRight', 'baseParentLeft', 'targetParentLeft', 'baseParentRight', 'targetParentRight',
-'diff.viewport', 'size', 'diff.left.relation', 'diff.right.relation', 'diff.parent.left.relation',
-'diff.parent.right.relation', 'diff.left.viewport', 'diff.right.viewport', 'alignment',
-'diff.parent.left.viewport', 'diff.parent.right.viewport', 'parent.alignment', 'diff.parent.y',
-'diff.parent.y.height', 'diff.height.height', 'diff.width.viewport', 'imageDiff.size',
-'chiSquared.size', 'imageDiff.size.viewport', 'chiSquared.size.viewport', 'pHashDistance.viewport',
+'diff.viewport', 'diff.left.relation', 'diff.right.relation', 'diff.parent.left.relation',
+'diff.parent.right.relation', 'alignment',
+'diff.parent.left.viewport', 'diff.parent.right.viewport', 'parent.alignment',
+'diff.parent.y.height',
+'imageDiff.size.viewport', 'chiSquared.size.viewport', 'pHashDistance.viewport',
     'size',  # features used in the article
     'diff.height.height',
     'diff.width.viewport',
@@ -62,86 +61,80 @@ X_cross = np.array([ dataset[:, index] for index in crosscheck_index ], dtype=fl
 y = np.array(dataset[:, class_index], dtype=int)
 urls = dataset[:, url_index]
 
-#random.seed(42)
-#model = tree.DecisionTreeClassifier(criterion='entropy', random_state=42)
-##model = ensemble.RandomForestClassifier(criterion='entropy', random_state=42)
-#X_new = X
-#rfecv = RFECV(model, cv=GroupKFold(n_splits=5), scoring='f1_macro')
-#rfecv.fit(X, y, groups=urls)
-#X_new = rfecv.transform(X)
-#print (X.shape)
-#print (X_new.shape)
-#headers.append('diff_out_viewport_left')
-#headers.append('diff_out_viewport_right')
-#features_index.append(headers.index('diff_out_viewport_left'))
-#features_index.append(headers.index('diff_out_viewport_right'))
-#print ([ headers[features_index[i]]
-#            for i in range(0, len(rfecv.ranking_))
-#            if rfecv.ranking_[i] == 1 ])
-#
-#params = {
-##    'n_estimators': [1, 5, 10, 20, 50],
-#    'criterion': ["gini", "entropy"],
-#    'max_depth': [10, 30, 60, 100, None],
-#    'min_samples_split': [2, 10, 30, 100, 200, 1000],
-#    'class_weight': [None, 'balanced']
-#}
-#cfl = GridSearchCV(model, params, cv=5)
-#cfl.fit(X_new, y)
-#for param, value in cfl.best_params_.items():
-#    print("%s : %s" % (param, value))
-#model = tree.DecisionTreeClassifier(random_state=42)
-##model = ensemble.RandomForestClassifier(random_state=42)
-#model.set_params(**cfl.best_params_)
-#
-#random.seed(42)
-#folds = GroupKFold(n_splits=10)
-#scores = cross_val_score(model, X_new, y, cv=folds, groups=urls, scoring='f1_macro')
-#print(scores)
-#print("OUR APPROACH F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
 random.seed(42)
 model = tree.DecisionTreeClassifier(criterion='entropy', random_state=42)
-folds = GroupKFold(n_splits=10)
-scores = cross_val_score(model, X_cross, y, cv=folds, groups=urls, scoring='f1_macro')
-#print(scores)
-print("CROSSCHECK   F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
-
-print('===== Feature selection =====')
-##model = tree.DecisionTreeClassifier(criterion='entropy')
-model = svm.LinearSVC()
-rfecv = RFECV(model, cv=StratifiedKFold(5), scoring='f1_macro')
+#model = ensemble.RandomForestClassifier(criterion='entropy', random_state=42)
+X_new = X
+rfecv = RFECV(model, cv=GroupKFold(n_splits=5), scoring='f1_macro')
 rfecv.fit(X, y, groups=urls)
 X_new = rfecv.transform(X)
 print (X.shape)
 print (X_new.shape)
+headers.append('diff_out_viewport_left')
+headers.append('diff_out_viewport_right')
+features_index.append(headers.index('diff_out_viewport_left'))
+features_index.append(headers.index('diff_out_viewport_right'))
 print ([ headers[features_index[i]]
             for i in range(0, len(rfecv.ranking_))
             if rfecv.ranking_[i] == 1 ])
 
 params = {
-    'C': [1, 5, 10, 20, 100],
-    'penalty': ["l1", "l2"],
-    'tol': [0.0001, 0.1, 1, 5],
-    'loss': ['hinge', 'square_hinge'],
+#    'n_estimators': [1, 5, 10, 20, 50],
+    'criterion': ["gini", "entropy"],
+    'max_depth': [10, 30, 60, 100, None],
+    'min_samples_split': [2, 10, 30, 100, 200, 1000],
     'class_weight': [None, 'balanced']
 }
 cfl = GridSearchCV(model, params, cv=5)
 cfl.fit(X_new, y)
 for param, value in cfl.best_params_.items():
     print("%s : %s" % (param, value))
+model = tree.DecisionTreeClassifier(random_state=42)
+#model = ensemble.RandomForestClassifier(random_state=42)
+model.set_params(**cfl.best_params_)
 
-#print (features_index)
-#print (rfecv.ranking_)
 random.seed(42)
-##model = tree.DecisionTreeClassifier(criterion='entropy', random_state=42)
-#model = svm.SVC(gamma='scale')
-model = svm.LinearSVC()
-model.set_paramams(**cfl.best_params_)
 folds = GroupKFold(n_splits=10)
 scores = cross_val_score(model, X_new, y, cv=folds, groups=urls, scoring='f1_macro')
 #print(scores)
-print("Our approach Feature Selection SVM F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
-#features.append('diff_out_viewport_left')
-#features.append('diff_out_viewport_right')
-#selected_index = [ features[i] for i in range(len(features)) if rfecv.ranking_[i] == 1]
-#print(selected_index)
+print("OUR APPROACH F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
+model = tree.DecisionTreeClassifier(criterion='entropy', random_state=42)
+#model = ensemble.RandomForestClassifier(criterion='entropy', random_state=42)
+cfl = GridSearchCV(model, params, cv=5)
+cfl.fit(X_cross, y)
+for param, value in cfl.best_params_.items():
+    print("%s : %s" % (param, value))
+random.seed(42)
+model = tree.DecisionTreeClassifier(criterion='entropy', random_state=42)
+#model = ensemble.RandomForestClassifier(criterion='entropy', random_state=42)
+model.set_params(**cfl.best_params_)
+folds = GroupKFold(n_splits=10)
+scores = cross_val_score(model, X_cross, y, cv=folds, groups=urls, scoring='f1_macro')
+#print(scores)
+print("CROSSCHECK   F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
+
+#print('===== SVM Hyperparameter Tunning =====')
+#X_new = X
+#model = svm.SVC()
+#
+#params = {
+#    'kernel': ['rbf', 'poly', 'sigmoid'],
+#    'C': [1, 10, 100],
+#    'gamma': ['scale', 'auto'],
+#    'degree': [1, 2, 3],
+#    'coef0': [0, 10, 100],
+#    'tol': [0.0001, 0.1, 1, 5],
+#    'class_weight': [None, 'balanced']
+#}
+#cfl = GridSearchCV(model, params, cv=5)
+#cfl.fit(X_new[1:1000,:], y[1:1000])
+#for param, value in cfl.best_params_.items():
+#    print("%s : %s" % (param, value))
+#
+#random.seed(42)
+#model = svm.SVC()
+#model.set_params(**cfl.best_params_)
+#folds = GroupKFold(n_splits=10)
+#scores = cross_val_score(model, X_new, y, cv=folds, groups=urls, scoring='f1_macro')
+##print(scores)
+#print("Our approach Feature Selection SVM F-measure: %s on average and %s SD" % (scores.mean(), scores.std()))
