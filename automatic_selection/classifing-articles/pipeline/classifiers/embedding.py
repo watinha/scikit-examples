e65f48f2597851a78aaa9e28729a157c49956b77
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2019-04-12 22:17:09 -0300

    now loading gensim embeddings for Software Engineering

--- automatic_selection/classifing-articles/pipeline/classifiers/embedding.py
+++ automatic_selection/classifing-articles/pipeline/classifiers/embedding.py
@@ -77,3 +77,52 @@ class MLPKerasGloveEmbeddingClassifier (EmbeddingClassifier):
             model.summary()
             return model
         return KerasClassifier(build_fn=create_model, epochs=150, verbose=0)
+
+
+class MLPKerasSEEmbeddingClassifier (EmbeddingClassifier):
+    def __init__ (self, seed=42, activation='relu', neurons_number=10, embedding_dim=200, maxlen=500, gensim_file='SO_vectors_200.bin'):
+        EmbeddingClassifier.__init__(self, seed)
+        self.classifier_name = 'MLPKerasSEEmbedding'
+        self._activation = activation
+        self._seed = seed
+        self._neurons_number = neurons_number
+        self._glove_file = glove_file
+        self._embedding_dim = embedding_dim
+        self._maxlen = maxlen
+        self._embedding_matrix = None
+
+
+    def get_classifier (self, X, y, word_index):
+        print('===== MLP Keras with %d hidden neuros and SE Embedding =====' % (self._neurons_number))
+        # generate embedding matrix
+        if (self._embedding_matrix == None):
+            se_embeddings = gensim.models.KeyedVectors.load_word2vec_format(
+                    './data/SO_vectors_200.bin', binary=True)
+            embedding_dim = self._embedding_dim
+            vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
+            self._embedding_matrix = np.zeros((vocab_size, embedding_dim))
+
+            for word in word_index.keys():
+                try:
+                    idx = word_index[word]
+                    self._embedding_matrix[idx] = np.array(
+                        se_embedding.get_vector(word), dtype=np.float32)[:embedding_dim]
+                except:
+                    print('%s not in embedding...' % (word))
+
+
+        def create_model ():
+            input_dim = X.shape[1]
+            model = Sequential()
+            model.add(layers.Embedding(input_dim=vocab_size,
+                                       output_dim=embedding_dim,
+                                       weights=[self._embedding_matrix],
+                                       input_length=self._maxlen,
+                                       trainable=True))
+            model.add(layers.GlobalMaxPool1D())
+            model.add(layers.Dense(self._neurons_number, activation='relu'))
+            model.add(layers.Dense(1, activation='sigmoid'))
+            model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+            model.summary()
+            return model
+        return KerasClassifier(build_fn=create_model, epochs=150, verbose=0)
